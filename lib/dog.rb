d673992c51c205a00d83af370c287befe1b79f0f
Francis-Muriithi <mufraggie@gmail.com>
2022-10-20 17:11:37 +0300

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,22 @@
+require "pry"
 class Dog
-
+    def name=(name)
+        @name = name
+    end
+    def breed=(breed)
+        @breed = breed
+    end
+    def name
+        @name
+    end
+    def breed
+        @breed
+    end
 end
+
+Fido = Dog.new 
+Fido.name ="Fido"
+snoopy= Dog.new
+snoopy.breed ="Beagle"
+
+binding.pry
